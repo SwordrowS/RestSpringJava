07560bee75b854ad4f7b2c70c77f4c3be395c689
DESKTOP-ODN2J8T\SwordrowS <dfmarques88@gmail.com>
2024-08-07 14:51:13 +0100

    Terminal migration implemented

--- RestSpringJava/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ RestSpringJava/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,8 +1,8 @@
 INSERT INTO `person` VALUES 
-	(1,'Liverpool','John','Male','Constantine'),
-	(2,'Hell\'s Kitchen','Matt','Male','Murdock'),
-	(4,'Hell\'s Kitchen','Frank','Male','Castle'),
-	(5,'New York','Stephen','Male','Strange');
+	(1,'John','Constantine','Liverpool','Male'),
+	(2,'Matt','Murdock','Hell\'s Kitchen','Male'),
+	(4,'Frank','Castle','Hell\'s Kitchen','Male'),
+	(5,'Stephen','Strange','New York','Male');
 
 
 
