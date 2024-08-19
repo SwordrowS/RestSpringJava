3523f484347593460e3b40183725ae82503985b9
DESKTOP-ODN2J8T\SwordrowS <dfmarques88@gmail.com>
2024-08-19 15:23:27 +0100

    Swagger Integrations test

--- RestSpringJava/src/test/java/swordrows/integrationtests/swagger/SwaggerIntegrationTest.java
+++ RestSpringJava/src/test/java/swordrows/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,13 +1,33 @@
-package swordrows;
+package swordrows.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AppTests {
+import swordrows.configs.TestConfigs;
+import swordrows.integrationtests.testcontainers.AbstractIntegrationTest;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
 
 	@Test
-	void contextLoads() {
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
 	}
 
 }
